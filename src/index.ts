dd95737d1680d030ed0285d2d4feec4cc34c1d5e
ubu11 <uvaisubu11@gmail.com>
2025-11-22 20:54:59 +0530

    project bot init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("done")
+import {Client,Events,GatewayIntentBits} from "discord.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+type clientType = {
+  readyClient: Client<boolean>
+}
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]})
+client.once(Events.ClientReady,(readyClient)=>{
+  console.log(`Logged in as ${readyClient.user.tag}`)
+})
+
+client.login(process.env.Discord_token)
